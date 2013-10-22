65638a5816290d3db9fa03dec9b81a7ad818c2e3
Dean De Carli <ddecarli@bu.edu>
2013-10-22 03:41:47 -0400

    updated GDP.__init__

--- GDP.py
+++ GDP.py
@@ -1,6 +1,25 @@
-class GDP:
-    time = 0
-    def __init__(self, floors, elevators, dTime):
-        self.floors = floorCount
-        self.elevators = elevatorCount
+defaultPeopleArray = []  ## [time][floor][dest][freq]
+
+class GDP():
+    def __init__(self, 
+                 floorNum = 9,
+                 ##primary floor?
+                 elevatorNum = 2,
+                 dTime = .11, 
+                 peopleFreqArray = defaultPeopleArray, 
+                 filename = 'default',
+                 doorDelay = 5,
+                 floorDelay = 2):
+################# Assign Default Parameters #####################
+        self.floorNum = floorNum
+        self.elevatorNum = elevatorNum
         self.dTime = dTime
+        self.peopleFreqArray = peopleFreqArray
+        if (filename == default):
+            self.filename = "GDP_result_" + str(floorNum) + "_" + str(elevatorNum)
+        else:
+            self.filename = filename
+        self.doorDelay = doorDelay
+        self.floorDelay = floorDelay
+
+        self.timePassed = 0
